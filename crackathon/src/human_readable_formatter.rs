adf5610f29d5d30a88b75fa677dd375e17f89d94
MordecaiMalignatus <mordecai@malignat.us>
2025-07-09 16:36:21 +0200

    crack: fix formatter, close to being right

--- crackathon/src/human_readable_formatter.rs
+++ crackathon/src/human_readable_formatter.rs
@@ -2,31 +2,110 @@ use serde_json::ser::CompactFormatter;
 use serde_json::ser::Formatter;
 use std::io;
 
-pub struct HumanReadableFormatter {}
+pub struct HumanReadableFormatter {
+    inside_archive: bool,
+    inside_list: bool,
+    inside_card: bool,
+    indent: usize,
+}
 
 impl HumanReadableFormatter {
     pub fn new() -> Self {
-        HumanReadableFormatter {}
+        HumanReadableFormatter {
+            inside_archive: false,
+            inside_list: false,
+            inside_card: false,
+            indent: 0,
+        }
     }
 }
 
 impl Formatter for HumanReadableFormatter {
+    fn begin_object<W>(&mut self, writer: &mut W) -> io::Result<()>
+    where
+        W: ?Sized + io::Write,
+    {
+        let c: String;
+        if !self.inside_archive {
+            self.inside_archive = true;
+            c = " ".repeat(self.indent) + "{\n";
+            self.indent += 2;
+        } else {
+            c = "{".to_string();
+        }
+
+        writer.write(c.as_bytes()).map(|_f| Ok(()))?
+    }
+
+    fn end_object<W>(&mut self, writer: &mut W) -> io::Result<()>
+    where
+        W: ?Sized + io::Write,
+    {
+        let c: String;
+        if !self.inside_archive {
+            self.inside_archive = false;
+            c = " ".repeat(self.indent) + "}\n";
+            self.indent -= 2;
+        } else {
+            c = "}\n".to_string();
+        }
+
+        writer.write(c.as_bytes()).map(|_f| Ok(()))?
+    }
+
+    fn begin_array_value<W>(&mut self, writer: &mut W, _first: bool) -> io::Result<()>
+    where
+        W: ?Sized + io::Write,
+    {
+        let c: String;
+        if !self.inside_card {
+            self.inside_card = true;
+            c = " ".repeat(self.indent).to_string();
+        } else {
+            c = String::new();
+        }
+
+        writer.write(c.as_bytes()).map(|_f| Ok(()))?
+    }
+
+    fn end_array_value<W>(&mut self, writer: &mut W) -> io::Result<()>
+    where
+        W: ?Sized + io::Write,
+    {
+        let c: String = if !self.inside_card {
+            "\n".to_string()
+        } else {
+            "".to_string()
+        };
+
+        writer.write(c.as_bytes()).map(|_f| Ok(()))?
+    }
+
     fn begin_array<W>(&mut self, writer: &mut W) -> io::Result<()>
     where
         W: ?Sized + io::Write,
     {
-        writer
-            .write(&"[\n".to_string().into_bytes())
-            .map(|_f| Ok(()))?
+        let c: String =  if !self.inside_card {
+            self.inside_list = true;
+            " ".repeat(self.indent - 2) + "[\n"
+        } else {
+            "[".to_string()
+        };
+
+        writer.write(c.as_bytes()).map(|_f| Ok(()))?
     }
 
     fn end_array<W>(&mut self, writer: &mut W) -> io::Result<()>
     where
         W: ?Sized + io::Write,
     {
-        writer
-            .write(&"]\n".to_string().into_bytes())
-            .map(|_f| Ok(()))?
+        let c: String = if self.inside_card {
+            self.inside_list = false;
+            " ".repeat(self.indent + 2) + "]"
+        } else {
+            "]\n".to_string()
+        };
+        writer.write(c.as_bytes()).map(|_f| Ok(()))?
     }
 
     // all these below are taken from CompactFormatter and passed through.
